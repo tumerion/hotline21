c58e63e06914c60924027710a9986b7c70ffc379
tumerion <59580116+tumerion@users.noreply.github.com>
2021-02-11 21:38:55 +0000

    Deploying to gh-pages from master @ 4b52e88b8589c2cefe98aba95521b80c32ff9794 🚀

--- text.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="text" tilewidth="32" tileheight="32" tilecount="625" columns="25">
- <image source="demo-tileset-v2.png" width="800" height="800"/>
-</tileset>
