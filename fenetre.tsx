d216f3ee129d5ea220d490ac552a39603e0ccfc2
tumerion <59580116+tumerion@users.noreply.github.com>
2021-02-13 17:22:49 +0000

    Deploying to gh-pages from master @ 514be42b529e96209ca40bf5c2d1ea3e57ba02c0 🚀

--- fenetre.tsx
+++ fenetre.tsx
@@ -1,4 +1,35 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="fenetre" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="tilesets_deviant_milkian_1.png" width="512" height="512"/>
+ <tile id="129">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="15.125" y="5" width="17.875" height="27.625"/>
+  </objectgroup>
+ </tile>
+ <tile id="130">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.625" y="6" width="33.25" height="26.25"/>
+  </objectgroup>
+ </tile>
+ <tile id="131">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.125" y="5.875" width="16.625" height="26.25"/>
+  </objectgroup>
+ </tile>
+ <tile id="145">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="15.625" y="-0.625" width="16.625" height="32.375"/>
+  </objectgroup>
+ </tile>
+ <tile id="146">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.25" y="-0.25" width="32.5" height="30.75"/>
+  </objectgroup>
+ </tile>
+ <tile id="147">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-1" y="-0.625" width="34.125" height="33.625"/>
+   <object id="2" x="-1.5" y="-0.625" width="17.5" height="32.375"/>
+  </objectgroup>
+ </tile>
 </tileset>
